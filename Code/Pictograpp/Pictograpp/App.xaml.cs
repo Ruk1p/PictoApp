6373946750ae8e5f076a86242a72a7cddf569f49
Ruk1p <rukip.85@gmail.com>
2022-09-10 17:00:37 -0300

    Cambios a la estructura de las carpetas

---
+++ Code/Pictograpp/Pictograpp/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Pictograpp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            MainPage = new NavigationPage(new Categories());
+            //MainPage = new Categories();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
