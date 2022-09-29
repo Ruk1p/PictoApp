273095452ea33844ac6b15debeaff0788aea40ea
Ruk1p <rukip.85@gmail.com>
2022-09-29 13:19:13 -0300

    Aplicacion con el prototipo de la feria de ciencias

--- Code/Pictograpp/Pictograpp/Images.xaml.cs
+++ Code/Pictograpp/Pictograpp/Images.xaml.cs
@@ -2,9 +2,10 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
-
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -13,14 +14,47 @@ namespace Pictograpp
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Images : ContentPage
     {
+        IEnumerable<Locale> locales;
+
         public Images()
         {
             InitializeComponent();
         }
+
+
         private async void NavigateButton_OnClicked(object sender, EventArgs e)
         {
             //await Navigation.PushAsync(new MainPage());
             await Navigation.PopAsync();
+        }
+
+        private async void Mama_Clicked(object sender, EventArgs e)
+        {
+
+            await TextToSpeech.SpeakAsync("Mama");
+
+        }
+        private async void Papa_Clicked(object sender, EventArgs e)
+        {
+            await TextToSpeech.SpeakAsync("Papá");
+        }
+        private async void Dormir_Clicked(object sender, EventArgs e)
+        {
+            await TextToSpeech.SpeakAsync("quiero dormir");
+        }
+        private async void TomarAgua_Clicked(object sender, EventArgs e)
+        {
+            await TextToSpeech.SpeakAsync("Quiero tomar agua");
+        }
+
+        private async void TengoCalor_Clicked(object sender, EventArgs e)
+        {
+            await TextToSpeech.SpeakAsync("Tengo Calor");
+        }
+        private async void Pintar_Clicked(object sender, EventArgs e)
+        {
+            await TextToSpeech.SpeakAsync("Quiero Pintar");
+
         }
     }
 }
